9c0586c3f683acc4ad72fc896d6f9d90ef42c5b3
lekisy <lekarabari12@gmail.com>
2023-07-26 12:38:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,29 @@
 
 /**
  * _strcat - concatenates two strings
- * 0dest: destination string
- * 0src: source string
- * Return: dest;
+ * @dest: input value
+ * @src: input value
+ * Return: void
  */
-
 char *_strcat(char *dest, char *src)
-{
-	int destlen = 0;
-	int srclen = 0;
-	int i;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
-		destlen++;
-	for (i = 0 ; src[i] != '\0' ; i++)
-		srclen++;
-	for (i = 0 ; i <= srclen ; i++)
-		dest[destlen + i] = src[i];
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
-}	
+}
